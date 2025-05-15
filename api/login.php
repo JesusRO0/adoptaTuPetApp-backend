e63c23a24ca291776bcbead0ba0514c292d990ed
JesusRO0 <78034035+JesusRO0@users.noreply.github.com>
2025-05-15 18:11:01 +0200

    Update login.php

--- api/login.php
+++ api/login.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . '/../config/database.php';
-require_once 'controllers/AuthController.php';
+require_once __DIR__ . '/../config/db.php';
+require_once __DIR__ . '/../controllers/AuthController.php';
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
